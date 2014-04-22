d60eb54b9df4375cb1d38efb1ac8a52897a9f12f
Markus Bader <markus.bader@mni.thm.de>
2014-04-22 15:17:05 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to natural-deduction
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/natural_deduction/core_test.clj
@@ -0,0 +1,7 @@
+(ns natural-deduction.core-test
+  (:require [clojure.test :refer :all]
+            [natural-deduction.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
