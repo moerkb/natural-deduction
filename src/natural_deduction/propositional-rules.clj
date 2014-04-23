8114262c694698cfce0c82f1ee800058ae82f692
Markus Bader <markus.bader@mni.thm.de>
2014-04-23 14:36:31 +0200

    propositional rules changed to new structure (pre/post/dir)

--- src/natural_deduction/propositional-rules.clj
+++ src/natural_deduction/propositional-rules.clj
@@ -2,54 +2,65 @@
 
 ; and
 (def and-i {:name "∧i"
-            :pre-top '[$1 $2]
-            :subs '[(and $1 $2)]})
+            :pre '[$1 $2]
+            :post '(and $1 $2)
+            :dir :both})
 
 (def and-e1 {:name "∧e1"
-             :pre-top '[(and $1 $2)]
-             :subs '[$1]})
+             :pre '[(and $1 $2)]
+             :post '$1
+             :dir :forward})
 
 (def and-e2 {:name "∧e2"
-             :pre-top '[(and $1 $2)]
-             :subs '[$2]})
+             :pre '[(and $1 $2)]
+             :post '$2
+             :dir :forward})
 
 ; or
 (def or-i1 {:name "∨i1"
-            :pre-top '[$1]
-            :subs '[(or $1 $2)]})
+            :pre '[$1]
+            :post '(or $1 $2)
+            :dir :both})
 
 (def or-i2 {:name "∨i2"
-            :pre-top '[$2]
-            :subs '[(or $1 $2)]})
+            :pre '[$2]
+            :post '(or $1 $2)
+            :dir :both})
 
 (def or-e {:name "∨e"
-           :pre-top '[(or $1 $2)]
-           :pre-bot '[$3]
-           :subs '[[$1 $2] [$1 $3]]})
+           :pre '[(or $1 $2) [$1 $3] [$2 $3]]
+           :post '$3
+           :dir :forward})
 
 ; implication
 (def impl-i {:name "→i"
-             :pre-bot '[(impl $1 $2)]
-             :subs '[[$1 $2]]})
+             :pre '[[$1 $2]]
+             :post '(impl $1 $2)
+             :dir :backward})
 
 (def impl-e {:name "→e, MP"
-             :pre-top '[$1 (impl $1 $2)]
-             :subs '[$2]})
+             :pre '[$1 (impl $1 $2)]
+             :post '$2
+             :dir :forward})
 
 ; negation
 (def not-i {:name "¬i"
-            :pre-bot '[(not $1)]
-            :subs '[[$1 :contradiction]]})
+            :pre '[[$1 :contradiction]]
+            :post '(not $1)
+            :dir :backward})
 
 (def not-e {:name "¬e"
-            :pre-top '[$1 (not $1)]
-            :subs '[:contradiction]})
+            :pre '[$1 (not $1)]
+            :post :contradiction
+            :dir :backward})
 
 ; reductio ad absurdum, contradiction
 (def raa {:name "RAA"
-          :pre-bot '[$1]
-          :subs '[[(not $1) :contradiction]]})
+          :pre '[[(not $1) :contradiction]]
+          :post '$1
+          :dir :backward})
 
 (def efq {:name "⊥e, EFQ"
-          :pre-top '[:contradiction]
-          :subs '[$1]})
+          :pre '[:contradiction]
+          :post '$1
+          :dir :both})
