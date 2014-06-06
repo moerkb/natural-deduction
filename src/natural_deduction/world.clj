2a0318f96eac730ccec3b13c9dab959b7365e945
Daniel Kirsten <daniel.kirsten@mni.thm.de>
2014-06-06 11:33:08 +0200

    Worked on wold
    * the function build world works with nested vectors
    * the pretty printer works with "nested worlds"

--- src/natural_deduction/world.clj
+++ src/natural_deduction/world.clj
@@ -3,24 +3,41 @@
 (def worlds (atom []))
 (def counter (atom 0))
 
+#_(defn build-world
+  ([world]
+    (build-world world false))
+  
+  ([world flag]
+    (let [f (first world)
+          n (next world)
+          fl (if flag
+               flag
+               (= f '⊢))
+          b (if (= f '⊢)
+              :todo
+              f)
+          hashset {:body b
+                   :hash (when (not= f '⊢) (swap! counter inc))
+                   :rule (when (not fl) :premise)}]
+      (if (first n)
+        (cons
+          hashset
+          (build-world n fl))
+        [hashset]))))
+
 (defn build-world
-  [world flag]
-  (let [f (first world)
-        n (next world)
-        fl (if flag
-             flag
-             (= f '⊢))
-        b (if (= f '⊢)
-            :todo
-            f)
-        hashset {:body b
-                 :hash (when (not= f '⊢) (swap! counter inc))
-                 :rule (when (not fl) :premise)}]
-    (if (first n)
-      (cons
-        hashset
-        (build-world n fl))
-      [hashset])))
+  [world]
+  (let [flag (atom false)]
+    (reset! counter 0)
+    (clojure.walk/postwalk
+      (fn [x]
+        (if (vector? x)
+          x
+          {:body (if (= x '⊢) (do (reset! flag true) :todo) x)
+           :hash (when (not= x '⊢) (swap! counter inc))
+           :rule (when (not @flag) :premise)}
+          ))
+      world)))
 
 (defn build-pretty-string
   [elem]
@@ -33,12 +50,26 @@
            (when r (str "\t" r))
            ")"))))
 
-(defn pretty-printer ; TODO: inner beweise darstellen
-  [world]
-  (doseq [elem world]
-    (println (build-pretty-string elem))))
+(defn pretty-printer
+  ([world]
+    (pretty-printer world 0))
+  
+  ([world lvl]
+    (doseq [elem world]
+      (if (vector? elem)
+        (do
+          (dotimes [_ lvl] (print "| "))
+          (dotimes [_ (- 40 lvl)] (print "--"))
+          (println)
+          (pretty-printer elem (inc lvl))
+          (dotimes [_ lvl] (print "| "))
+          (dotimes [_ (- 40 lvl)] (print "--"))
+          (println))
+        (do
+          (dotimes [_ lvl] (print "| "))
+          (println (build-pretty-string elem)))))))
 
-(defn undo
+(defn undo!
   []
   (do
     (when (second @worlds) (reset! worlds (vec (drop-last @worlds))))
@@ -48,6 +79,6 @@
   [new-world]
   (do
     (reset! counter 0)
-    (reset! worlds [(vec (build-world new-world false))])
+    (reset! worlds [(vec (build-world new-world))])
     (pretty-printer (last @worlds))))
   
